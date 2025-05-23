18ade0c215ce05515748e7fbe2dbc0bbc5a03c75
Anurag <phoebbbey@gmail.com>
2025-05-23 20:15:01 +0545

    Surveillance record forms were added

--- surveillance/forms.py
+++ surveillance/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import SurveillanceRecord
+from .models import SurveillanceRecord # type: ignore
 
 class SurveillanceRecordForm(forms.ModelForm):
     class Meta:
