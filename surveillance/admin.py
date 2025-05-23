e3507c7d0afbd0635e1be60dd7477226946a6bf2
Niraj Pokhrel <s373345@students.cdu.edu.au>
2025-05-23 20:58:25 +0930

    Register Grower, PlantPart, and SurveillanceRecord in admin panel

--- surveillance/admin.py
+++ surveillance/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Grower, PlantPart, SurveillanceRecord
 
 # Register your models here.
+admin.site.register(Grower)
+admin.site.register(PlantPart)
+admin.site.register(SurveillanceRecord)
